1981412d61f8661f51bbb3036d664553396bfbb8
Jonas Natten <jonas.natten@knowit.no>
2017-11-30 08:09:51 +0100

    #682: Better way of escaping html characters in metaDescription

--- src/test/scala/no/ndla/articleimport/service/converters/HTMLCleanerTest.scala
+++ src/test/scala/no/ndla/articleimport/service/converters/HTMLCleanerTest.scala
@@ -704,4 +704,11 @@ class HTMLCleanerTest extends UnitSuite with TestEnvironment {
     result.content should equal(expectedContent)
   }
 
+  test("HTML characters are escaped in meta description even if they are in html tags") {
+    val content = TestData.sampleContent.copy(content = "", metaDescription ="""Hei dette er et mindre enn tegn &lt;start&gt; nice""")
+    val Success((result, _)) = htmlCleaner.convert(content, defaultImportStatus)
+
+    result.metaDescription should equal("Hei dette er et mindre enn tegn &lt;start&gt; nice")
+  }
+
 }
