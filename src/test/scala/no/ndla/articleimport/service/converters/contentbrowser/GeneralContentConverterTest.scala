68475f78627505172fc3938762350a0816c3be87
Henrik Nårstad <henrik.narstad@knowit.no>
2018-01-24 14:33:20 +0100

    fix failing test #863

--- src/test/scala/no/ndla/articleimport/service/converters/contentbrowser/GeneralContentConverterTest.scala
+++ src/test/scala/no/ndla/articleimport/service/converters/contentbrowser/GeneralContentConverterTest.scala
@@ -92,7 +92,7 @@ class GeneralContentConverterTest extends UnitSuite with TestEnvironment {
   test("That GeneralContentConverter inserts the content if insertion mode is 'lightbox_large'") {
     val contentString = s"[contentbrowser ==nid=$nodeId==imagecache=Fullbredde==width===alt=$altText==link===node_link=1==link_type=link_to_content==lightbox_size===remove_fields[76661]=1==remove_fields[76663]=1==remove_fields[76664]=1==remove_fields[76666]=1==insertion=lightbox_large==link_title_text===link_text=Tittel==text_align===css_class=contentbrowser contentbrowser]"
     val content = ContentBrowserString(contentString, "nb")
-    val expectedResult = s""" <$ResourceHtmlEmbedTag data-content-id="1" data-link-text="Tittel" data-resource="content-link" />"""
+    val expectedResult = s""" <$ResourceHtmlEmbedTag data-content-id="1" data-link-text="Tittel" data-open-in="new-context" data-resource="content-link" />"""
 
     when(extractService.getNodeGeneralContent(nodeId)).thenReturn(Seq(sampleFagstoff1, sampleFagstoff2))
     when(draftApiClient.getArticleIdFromExternalId(nodeId)).thenReturn(Some(sampleArticle.id))
