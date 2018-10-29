881280b6304950adf7de8cee71c1ad06607c6b37
Henrik Nårstad <henrik.narstad@knowit.no>
2018-10-29 15:29:06 +0100

    show author even if concept has no license #1386

--- src/main/scala/no/ndla/articleimport/model/domain/package.scala
+++ src/main/scala/no/ndla/articleimport/model/domain/package.scala
@@ -8,9 +8,9 @@ package no.ndla.articleimport.model
 
 package object domain {
 
-  def emptySomeToNone(lang: Option[String]): Option[String] = {
-    lang.filter(_.nonEmpty)
-  }
+  def emptySomeToNone(lang: Option[String]): Option[String] = lang.filter(_.nonEmpty)
+
+  def emptyStringToNone(str: String): Option[String] = emptySomeToNone(Option(str.trim))
 
   case class ArticleIds(articleId: Long, externalId: List[String], importId: Option[String])
   case class ImportId(importId: Option[String])
